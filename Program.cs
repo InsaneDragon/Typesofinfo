f38fbadd6a2cb180895f2433a94a7f253fdc050f
C#Developer <shahzodkiller11@gmail.com>
2020-03-21 19:06:49 +0500

    Info

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace infotypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
